d2a9e9d891fd98c8b6c075597c2fb592f82c50b8
vkahara <valtterikahara@gmail.com>
2023-03-31 11:33:13 +0300

    added better text rendering with different font sizes

--- main.py
+++ main.py
@@ -22,9 +22,15 @@ all_sprites.add(player)
 all_sprites.add(enemy)
 clock = pygame.time.Clock()
 
-def text_objects(text, font):
-    textSurface = font.render(text, True, (255,255,255))
-    return textSurface, textSurface.get_rect()
+small_font = pygame.font.SysFont(None, 20)
+big_font = pygame.font.SysFont(None, 40)
+
+def draw_text(text, font, color, surface, x, y):
+    text_object = font.render(text, 1, color)
+    text_rect = text_object.get_rect()
+    text_rect.topleft = (x, y)
+    surface.blit(text_object, text_rect)
+    
 
 def start_menu():
     menu = True
@@ -36,15 +42,12 @@ def start_menu():
                 
             if event.type == pygame.KEYDOWN:
                 return
-                
-        screen.fill((69,69,69))
-        smallText = pygame.font.Font('freesansbold.ttf', 20)
-        TextSurf, TextRect = text_objects("press any key to start race!", smallText)
-        TextRect.center = ((640/2),(100))
-        screen.blit(TextSurf, TextRect)
+            
+            
+        screen.fill((69,69,69))    
+        draw_text("drag_racing", big_font, (0,0,0), screen, 20 , 20)
         pygame.display.update()
         clock.tick(15)
-
 
 start_menu()
 while True:
@@ -53,7 +56,6 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-            
             
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:        
