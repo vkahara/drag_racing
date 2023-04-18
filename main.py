c8726fd85272cbde1bafe5d7c248c550ab0a2ad5
vkahara <valtterikahara@gmail.com>
2023-04-18 15:51:03 +0300

    added first test of starting lights

--- main.py
+++ main.py
@@ -9,76 +9,101 @@ import sys
 import os
 
 # initialize pygame, create a screen and set its size to 640x640
-# set window caption 
+# set window caption
 pygame.init()
 screen = pygame.display.set_mode((640, 640))
 pygame.display.set_caption("Drag Racing!")
 
 clock = pygame.time.Clock()
 
-#define small and big fonts and set them to system default font
+# define small and big fonts and set them to system default font
 small_font = pygame.font.SysFont(None, 28)
 big_font = pygame.font.SysFont(None, 60)
 
-#function to display text to screen
+# function to display text to screen
 def draw_text(text, font, color, surface, x, y):
     text_object = font.render(text, 1, color)
     text_rect = text_object.get_rect()
     text_rect = (x, y)
     surface.blit(text_object, text_rect)
-    
-#start menu function contains start menu as a separate game loop
-#start menu draws text and menu background to screen
-#start menu is used to select player car
+
+# start menu function contains start menu as a separate game loop
+# start menu draws text and menu background to screen
+# start menu is used to select player car
 def start_menu():
     car_preview = 0
-    car_preview_image = pygame.image.load((os.path.join("images", "ford_scorpio.png")))
-    car_preview_image = pygame.transform.scale(car_preview_image, (168,354))
+    car_preview_image = pygame.image.load(
+        (os.path.join("images", "ford_scorpio.png")))
+    car_preview_image = pygame.transform.scale(car_preview_image, (168, 354))
     while True:
         for event in pygame.event.get():
-            global chosen_car #global variable chosen car is assigned value in the menu
-            garage_background = pygame.image.load(os.path.join("images", "garage_background.png"))
-            garage_background = pygame.transform.scale(garage_background, (640,640))
-            screen.blit(garage_background, (0,0))
-            draw_text("select car", big_font, (0,0,0), screen, 215 , 530)
-            
+            global chosen_car  # global variable chosen car is assigned value in the menu
+            garage_background = pygame.image.load(
+                os.path.join("images", "garage_background.png"))
+            garage_background = pygame.transform.scale(
+                garage_background, (640, 640))
+            screen.blit(garage_background, (0, 0))
+            draw_text("select car", big_font, (0, 0, 0), screen, 215, 530)
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-                
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     if car_preview < (len(cars_module.cars_dict)-1):
                         car_preview += 1
                     else:
-                        car_preview = car_preview - (len(cars_module.cars_dict)-1)
-                        
+                        car_preview = car_preview - \
+                            (len(cars_module.cars_dict)-1)
+
                 if event.key == pygame.K_LEFT:
                     car_preview -= 1
                     if car_preview < 0:
-                        car_preview = car_preview + (len(cars_module.cars_dict)-1)
-                    
-                if event.key == pygame.K_RETURN:   
+                        car_preview = car_preview + \
+                            (len(cars_module.cars_dict)-1)
+
+                if event.key == pygame.K_RETURN:
                     chosen_car = cars_module.get_car_key(car_preview)
                     return
-                
-        car_preview_image = pygame.image.load((os.path.join("images", cars_module.cycle_images(car_preview))))
-        car_preview_image = pygame.transform.scale(car_preview_image, (168,354))       
-        
-        screen.blit(car_preview_image, (235, 150))      
-        
-        draw_text((cars_module.cycle_names(car_preview)), small_font, (0,0,0), screen, 260, 570)
+
+        car_preview_image = pygame.image.load(
+            (os.path.join("images", cars_module.cycle_images(car_preview))))
+        car_preview_image = pygame.transform.scale(
+            car_preview_image, (168, 354))
+
+        screen.blit(car_preview_image, (235, 150))
+
+        draw_text((cars_module.cycle_names(car_preview)),
+                  small_font, (0, 0, 0), screen, 260, 570)
         pygame.display.update()
         clock.tick(15)
-        
+
+
 def mainloop():
-    while True:
-        events = pygame.event.get()
-        for event in events:
+    race_start = False
+    all_sprites.draw(screen)
+
+    draw_text("Ready", big_font, (0,0,0), screen, 320 , 320)
+    pygame.display.update()
+    pygame.time.wait(3000)
+
+    draw_text("Set", big_font, (0,0,0), screen, 320 , 280)
+    pygame.display.update()
+    pygame.time.wait(3000)
+    
+    draw_text("go", big_font, (0,0,0), screen, 320 , 250)
+    pygame.display.update()
+    pygame.time.wait(3000)
+    
+    
+
+    while race_start:
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-                
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:        
                     player.control(player.get_speed())
@@ -103,12 +128,12 @@ def mainloop():
                 if event.key == pygame.K_SPACE:
                     player.set_nitrous_on(False)
                     
+
+        print(player.get_y())
         
-        print(player.get_y())                    
-        screen.fill((33,170,33))
+        pygame.display.flip()                        
         all_sprites.draw(screen)
         all_sprites.update() 
-        pygame.display.flip()    
         clock.tick(28)
     
 
