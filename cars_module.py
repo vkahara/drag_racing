fdf0726ae71f713d0cfcf37b623f9b30bf08d269
Jimi <jimi.hietakangas@edu.turkuamk.fi>
2023-04-19 20:28:10 +0300

    added myself as a car

--- cars_module.py
+++ cars_module.py
@@ -4,12 +4,14 @@ ford_scorpio = car.Car(0, "Ford", "Scorpio", 1, 20, "ford_scorpio.png")
 mitsubishi_evo = car.Car(1, "Mitshubishi", "Lancer Evo VI", 3, 10, "mitsubishi_evo.png")
 toyota_supra = car.Car(2, "Toyota", "Supra", 5, 10, "meitsi.jpg")
 volkswagen_golf = car.Car(3, "Volkswagen", "Golf", 2, 15, "volkswagen_golf.png")
+jumitus_auto = car.Car(4, "Nissan", "Almera", 1, 5, "jumi.png")
 
 cars_dict = {
     "ford_scorpio": ford_scorpio,
     "mitsubishi_evo": mitsubishi_evo,
     "toyota_supra": toyota_supra,
-    "volkswagen_golf": volkswagen_golf
+    "volkswagen_golf": volkswagen_golf,
+    "jumitus_auto": jumitus_auto
 }
 
 
